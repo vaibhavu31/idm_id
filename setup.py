766f2da60c27c74ed950d989826c8e7cf5e414b7
vaibhavu31 <vaibhavu31@gmail.com>
2021-11-13 15:49:21 +0100

    comments: committing my first package

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from setuptools import find_packages
-setup(name='idm_id',
+setup(name='ky83kyje',
  version='0.1',
  author='DSSS',
- author_email='name@fau.de',
+ author_email='vaibhav.uppal@fau.de',
  packages=find_packages(),
  install_requires=['numpy', 'Pillow', 'ipywidgets'])
